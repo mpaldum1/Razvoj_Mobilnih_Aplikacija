7d6fbd49b0ed3b015390d422ab7033bea54a268a
mpaldum1 <mpaldum1@etf.unsa.ba>
2019-04-07 09:04:49 +0200

    Kreiran KategorijaAdapter

---
+++ app/src/main/java/ba/unsa/etf/rma/klase/KategorijaAdapter.java
@@ -0,0 +1,43 @@
+package ba.unsa.etf.rma.klase;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import ba.unsa.etf.rma.R;
+
+public class KategorijaAdapter extends ArrayAdapter<Kategorija> {
+    public KategorijaAdapter(Context context, ArrayList<Kategorija> kategorije) {
+        super(context,0, kategorije);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    private View initView(int position, View convertView, ViewGroup parent){
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.spinner_layout,parent,false);
+        }
+        TextView nazivKategorije = convertView.findViewById(R.id.twSpinner);
+        Kategorija currentKategorija = getItem(position);
+
+        if(currentKategorija != null){
+            nazivKategorije.setText(currentKategorija.getNaziv());
+        }
+
+        return  convertView;
+    }
+
+}
